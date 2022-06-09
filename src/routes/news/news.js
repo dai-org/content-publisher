611f1e79926b307e1992892bb8a4353e3463abb7
Andrew Dunn <aldunn@aeyon.us>
2022-06-09 13:26:12 -0500

    Edit News

--- src/routes/news/news.js
+++ src/routes/news/news.js
@@ -1,9 +1,10 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import { getFirestore, collection, addDoc, onSnapshot, query, serverTimestamp } from 'firebase/firestore'
+import { getFirestore, collection, addDoc, onSnapshot, query, updateDoc, doc, serverTimestamp } from 'firebase/firestore'
 import './news.css'
 import NewsTable from './news-table';
+import { useAuth } from "../../components/provideAuth";
 
 // TODO: Invert progress bar text color as bar fills, see post [https://stackoverflow.com/a/61353195]
 
@@ -19,6 +20,29 @@ function News() {
     const [searchQuery, setSearchQuery] = useState('');
     const [posts, setPosts] = useState([]);
     const status = useRef();
+    const [AppUser, setAppUser] = useState([]);
+    const auth = useAuth();
+
+    useEffect(() => {
+        if (auth.user.email) {
+            const db = getFirestore();
+            const q = query(collection(db, "appUsers"), where('email', '==', auth.user.email));
+            const unsubscribe = onSnapshot(q, (querySnapshot) => {
+                const items = [];
+                
+                querySnapshot.forEach((doc) => {
+                    items.push(doc);
+                });
+
+                console.log(items[0].data());
+
+                setFormLoading(false);
+                setAppUser(items[0].data());
+            });
+
+            return unsubscribe;
+        }
+    }, [auth]);
 
     useEffect(() => {
         const db = getFirestore();
@@ -108,12 +132,10 @@ function News() {
                             type='button'
                             className='btn btn-success w-100 round-10'
                             ref={uploadButton}
-                            onClick={async (event) => {
-                                console.log(serverTimestamp());
-                                
+                            onClick={async (event) => {                                
                                 // Create Firestore document, holds file metadata
                                 const db = getFirestore();
-                                const docRef = await addDoc(collection(db, 'posts'), {
+                                const data = {
                                     subject: subject.current.value,
                                     body: body.current.value,
                                     video: video.current.value,
@@ -123,13 +145,14 @@ function News() {
                                     publishedOn: serverTimestamp(), 
                                     status: status.current.value,
 
-                                });
+                                };
 
                                 if (status.current.value === 'Approved') {
                                     data.approvedBy = AppUser.name;
                                     data.approvedOn = serverTimestamp();
                                     data.status = 'Approved';
                                 }
+                                const docRef = await addDoc(collection(db, 'posts'), data);
                                 console.log('Document written with ID: ', docRef.id);
 
                                 // Reset fields
@@ -152,7 +175,7 @@ function News() {
                             <strong>Posts awaiting approval ({posts.filter(entry => entry.data().status === 'Awaiting Approval').length})</strong>
                         </div> 
                         {
-                            faqs
+                            posts
                             .filter(entry => entry.data().status === 'Awaiting Approval')
                             .map(entry => {
                                 const { id } = entry;
@@ -208,7 +231,7 @@ function News() {
                         }
                     </div>
                 }
-                <h4 className={`text-start${faqs.length !== 0 ? ' mb-4' : ' mb-0'}`}>Posts ({posts.length})</h4>
+                <h4 className={`text-start${posts.length !== 0 ? ' mb-4' : ' mb-0'}`}>Posts ({posts.length})</h4>
                 <div className='d-flex justify-content-start filter-container'>
                 <div className='search-container'>
                         <FontAwesomeIcon icon={faSearch} className='search-icon' />

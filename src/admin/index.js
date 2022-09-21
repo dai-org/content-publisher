9e1adf75155cb175279ac8ab94bcb32272917c28
Andrew Dunn <aldunn@aeyon.us>
2022-09-21 10:58:10 -0500

    added new functions

--- src/admin/index.js
+++ src/admin/index.js
@@ -24,6 +24,7 @@ export async function handleClick(from, to, subject, html) {
   formData.append('to', to);
   formData.append('subject', subject);
   formData.append('html', html);
+  formData.append('api', "DC-IUYTFGHJ-IUYG876-VVBY986-CP");
   fetch('http://dunn-carabali.com/fetch.php', {
     method: 'POST', 
     mode: 'no-cors', 

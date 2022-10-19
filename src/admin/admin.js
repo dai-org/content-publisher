446c6ab281a92d4905359c36ea1df8f023fcaa92
Andrew Dunn <aldunn@aeyon.us>
2022-10-19 15:57:44 -0500

    adjust style

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -244,7 +244,7 @@ function Admin() {
                                         </button>
                                         <button
                                 type='button'
-                                style={{marginLeft:5}}
+                                style={{marginLeft:5, marginRight:5}}
                                 className='btn btn-danger w-33 btn-sm round-10'
                                 ref={uploadButton}
                                 onClick={async (event) => {
